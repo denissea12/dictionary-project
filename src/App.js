f0071e4904ecbedef5d6251841f3ec83b41c92de
denissea12 <denissea.12@gmail.com>
2025-04-27 13:54:43 -0500

    added basic formatting for search engine

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
-function App() {
-  return <div className="App">Hello</div>;
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <header className="app-header"></header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">Coded by SheCodes</footer>
+      </div>
+    </div>
+  );
 }
-
-export default App;
